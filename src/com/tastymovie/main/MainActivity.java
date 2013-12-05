67e7969192e929e4d471588dee1f47efe8eb440e
xavier <xavier@xavier-HP-ProBook-6570b>
2013-12-05 22:56:37 +0100

    choice_java

--- src/com/tastymovie/main/MainActivity.java
+++ src/com/tastymovie/main/MainActivity.java
@@ -2,7 +2,11 @@ package com.tastymovie.main;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +14,17 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		
+		final Button loginButton = (Button) findViewById(R.id.btn_search);
+		  loginButton.setOnClickListener(new OnClickListener() {
+					
+		  @Override
+		  public void onClick(View v) {
+			Intent intent = new Intent(MainActivity.this, Choice.class);
+			startActivity(intent);
+			}
+		});
 	}
 
 	@Override
