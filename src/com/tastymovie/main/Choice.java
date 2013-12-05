efc41f31670fe2448e43492f0e7dcdc365d10bdd
noel <noel@noel-pc>
2013-12-06 00:17:08 +0100

    Visibility 3 movies

--- src/com/tastymovie/main/Choice.java
+++ src/com/tastymovie/main/Choice.java
@@ -2,7 +2,12 @@ package com.tastymovie.main;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class Choice extends Activity {
 
@@ -10,6 +15,38 @@ public class Choice extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.choice);
+		
+		final Button btn_movie1 = (Button) findViewById(R.id.btn_movie1);
+		final Button btn_movie2 = (Button) findViewById(R.id.btn_movie2);
+		final Button btn_movie3 = (Button) findViewById(R.id.btn_movie3);
+		final LinearLayout ll_movie1 = (LinearLayout) findViewById(R.id.ll_movie1);
+		final LinearLayout ll_movie2 = (LinearLayout) findViewById(R.id.ll_movie2);
+		final LinearLayout ll_movie3 = (LinearLayout) findViewById(R.id.ll_movie3);
+		
+		btn_movie1.setOnClickListener(new OnClickListener() {					
+		  @Override
+		  public void onClick(View v) {
+			  ll_movie1.setVisibility(LinearLayout.VISIBLE);
+			  ll_movie2.setVisibility(LinearLayout.GONE);
+			  ll_movie3.setVisibility(LinearLayout.GONE);
+			}
+		});
+		btn_movie2.setOnClickListener(new OnClickListener() {					
+			  @Override
+			  public void onClick(View v) {
+				  ll_movie1.setVisibility(LinearLayout.GONE);
+				  ll_movie2.setVisibility(LinearLayout.VISIBLE);
+				  ll_movie3.setVisibility(LinearLayout.GONE);
+				}
+			});
+		btn_movie3.setOnClickListener(new OnClickListener() {					
+			  @Override
+			  public void onClick(View v) {
+				  ll_movie1.setVisibility(LinearLayout.GONE);
+				  ll_movie2.setVisibility(LinearLayout.GONE);
+				  ll_movie3.setVisibility(LinearLayout.VISIBLE);
+				}
+			});
 	}
 
 	@Override
